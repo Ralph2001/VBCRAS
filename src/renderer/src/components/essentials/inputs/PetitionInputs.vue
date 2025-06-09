d6c4ad375f4ee51a53cd19f2c9fce18a2516c686
Ralph2001 <villanuevaralph2001@gmail.com>
2025-06-09 11:38:41 +0800

    Refactor code structure for improved readability and maintainability

--- src/renderer/src/components/essentials/inputs/PetitionInputs.vue
+++ src/renderer/src/components/essentials/inputs/PetitionInputs.vue
@@ -3,16 +3,16 @@
     <input :value="type" disabled :class="[props.type === 'CCE' ? 'bg-[#4A90E2]' : 'bg-[#2C7A7B]']"
       class=" select-none border border-e-0 font-bold text-sm block w-full text-white  p-2.5" />
 
-    <input  :value="props.petition_number_value"
-      @input="input_petition_number($event.target.value)"
-      ref="input"
+    <input @keydown.tab="handleTab" @keydown.enter="focusNextInput" @keydown.up="focusPreviousInput"
+      @keydown.down="focusNextInput" :value="props.petition_number_value"
+      @input="input_petition_number($event.target.value)" ref="input"
       class="bg-gray-50 border outline-none ring-0   border-s-0 border-e-0 items-center font-bold text-gray-800 text-sm border-t-2 border-b-2 focus:ring-green-500 focus:border-green-500 flex justify-center text-center w-full active:ring-green-500 p-2.5" />
 
     <input disabled :tabindex="skip ? '-1' : '0'" @input="emit('type-year', $event.target.value)"
       :class="[props.republic_act === '10172' ? 'border-e-transparent' : '']" v-model="year"
-      class="bg-gray-50 border outline-none ring-0  border-s-0 font-bold text-gray-800 text-sm focus:ring-green-500 focus:border-green-500 flex justify-center text-center w-full active:ring-green-500 p-2.5" />
+      class=" border  disabled:cursor-not-allowed disabled:bg-gray-200 outline-none ring-0  border-s-0 font-bold text-gray-800 text-sm focus:ring-green-500 focus:border-green-500 flex justify-center text-center w-full active:ring-green-500 p-2.5" />
     <input value="R.A. 10172" disabled v-if="props.republic_act === '10172'"
-      class="border font-bold border-s-transparent bg-gray-200 select-none text-gray-500 text-sm focus:ring-green-500 focus:border-green-500 w-full flex justify-center text-center active:ring-green-500 p-2.5" />
+      class="border font-bold disabled:cursor-not-allowed border-s-transparent bg-gray-200 select-none text-gray-500 text-sm focus:ring-green-500 focus:border-green-500 w-full flex justify-center text-center active:ring-green-500 p-2.5" />
   </div>
 </template>
 <script setup>
@@ -22,8 +22,8 @@ const emit = defineEmits(['type-petition-number', 'type-year'])
 const input = ref(null);
 
 onMounted(() => {
- 
-    input.value.focus();
+
+  input.value.focus();
 
 });
 
@@ -41,7 +41,7 @@ const props = defineProps({
 
 
 const type = computed(() => {
-  if(props.migrant){
+  if (props.migrant) {
     return 'MP' + props.type
   }
   return props.type
@@ -50,5 +50,42 @@ const type = computed(() => {
 const input_petition_number = (e) => {
   emit('type-petition-number', e)
 }
+
+const focusPreviousInput = (event) => {
+  event.preventDefault();
+
+  // Select only focusable elements (input, button, and elements with tabindex >= 0)
+  const inputs = Array.from(document.querySelectorAll('input, button, [tabindex]'))
+    .filter(input => input.tabIndex >= 0);
+
+  const index = inputs.indexOf(event.target);
+  if (index > 0) {
+    inputs[index - 1].focus();
+  }
+}
+const handleTab = (event) => {
+  if (props.type === 'date' && !event.shiftKey) {
+    event.preventDefault();
+    focusNextInput(event);
+
+  }
+};
+
+const focusNextInput = (event) => {
+  if (props.skipnext) {
+    return;
+  }
+  event.preventDefault();
+
+  // Select only focusable elements (input, button, and elements with tabindex >= 0)
+  const inputs = Array.from(document.querySelectorAll('input, button, [tabindex]'))
+    .filter(input => input.tabIndex >= 0);
+
+  const index = inputs.indexOf(event.target);
+  if (index >= 0 && index < inputs.length - 1) {
+    inputs[index + 1].focus();
+  }
+}
+
 
 </script>
